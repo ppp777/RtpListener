fc96b20cd1b612a292008bb308f01e1bc5afddd7
ppp777 <pasha_z1@mail.ru>
2013-10-12 00:07:15 +0400

    test1

--- src/com/testhome/JPH1.java
+++ src/com/testhome/JPH1.java
@@ -2,12 +2,15 @@ package com.testhome;
 
 import org.jnetpcap.packet.JPacket;
 import org.jnetpcap.packet.JPacketHandler;
+import org.jnetpcap.packet.format.FormatUtils;
 import org.jnetpcap.protocol.network.Ip4;
 import org.jnetpcap.protocol.tcpip.Http;
 import org.jnetpcap.protocol.tcpip.Tcp;
 import org.jnetpcap.protocol.tcpip.Udp;
 import org.jnetpcap.protocol.voip.Rtp;
 import org.jnetpcap.protocol.voip.Sip;
+
+import javax.swing.*;
 
 public class JPH1 implements JPacketHandler<StringBuilder> {
 
@@ -17,43 +20,57 @@ public class JPH1 implements JPacketHandler<StringBuilder> {
         final Sip sip1 = new Sip();
         final Http http = new Http();
         final Rtp rtp1 = new Rtp();
+        JTextPane textPane1;
+    JPH1(JTextPane jTextPane){
+        this.textPane1 = jTextPane;
+    }
     @Override
     public void nextPacket(JPacket packet, StringBuilder errbuf) {
 
         String source = "";
         String destination = "";
-/*
-                packet.getHeader(ipv4);
-                if (!(ipv4.source()).equals(null)){
-                    source = FormatUtils.ip(ipv4.source());
-                }
-                if  (!(ipv4.destination()).equals(null)){
-                    destination = FormatUtils.ip(ipv4.destination());
-                }
+        String printResult = "";
+        packet.getHeader(ipv4);
+        if (!(ipv4.source()).equals(null)){
+            source = FormatUtils.ip(ipv4.source());
+        }
+        if  (!(ipv4.destination()).equals(null)){
+            destination = FormatUtils.ip(ipv4.destination());
+        }
+        try {
+            //textPane1.setText(textPane1.getText() + "> Source ip = " + source + ";  Destination ip = " + destination + "\n");
+            printResult = "> Source ip = " + source + ";  Destination ip = " + destination + "\n";
+        }
+        catch (Exception e){
+            printResult = printResult + "!!! Exception ip IP Addr" + e.getCause() + "\n";
+            textPane1.setText(textPane1.getText() + printResult);
+            textPane1.setCaretPosition(textPane1.getDocument().getLength());
+        }
+        //System.out.println("From " + source + " To " + destination);
 
-                System.out.println("From " + source + " To " + destination);
-*/
         if (packet.hasHeader(Rtp.ID)) {
             packet.getHeader(rtp1);
-            System.out.printf("rtp1.csrcLength() = " + rtp1.csrcLength());
-            System.out.printf("rtp1.csrc() = " + rtp1.csrc());
-            System.out.printf("rtp1.ssrc()" + rtp1.ssrc());
+            printResult = printResult + "> rtp1.csrcLength() = " + rtp1.csrcLength() + ";  rtp1.csrc() = " + rtp1.csrc() + "; rtp1.ssrc()" + rtp1.ssrc() + "\n";
+//            textPane1.setText(textPane1.getText() + "> rtp1.csrcLength() = " + rtp1.csrcLength() + ";  rtp1.csrc() = " + rtp1.csrc() + "; rtp1.ssrc()" + rtp1.ssrc() + "\n");
+//            System.out.printf("rtp1.csrcLength() = " + rtp1.csrcLength() + ";  rtp1.csrc() = " + rtp1.csrc() + "; rtp1.ssrc()" + rtp1.ssrc());
         }
 
-/*
-                if (packet.hasHeader(Tcp.ID)) {
-                    packet.getHeader(tcp);
-                    System.out.printf("tcp.dst_port=%d%n", tcp.destination());
-                    System.out.printf("tcp.src_port=%d%n", tcp.source());
-                    System.out.printf("tcp.ack=%x%n", tcp.ack());
-                }
-                if (packet.hasHeader(Udp.ID)) {
-                    packet.getHeader(udp1);
-                    System.out.printf("udp.dst_port=%d%n", udp1.destination());
-                    System.out.printf("udp.src_port=%d%n", udp1.source());
-                }
-*/
-        System.out.printf("frame #%d%n", packet.getFrameNumber());
+
+        if (packet.hasHeader(Tcp.ID)) {
+            packet.getHeader(tcp);
+            printResult = printResult + "> tcp.dst_port = " + tcp.destination() + "; tcp.src_port = " + tcp.source() + "; tcp.ack = " + tcp.ack()  + "\n";
+//            textPane1.setText(textPane1.getText() + "> tcp.dst_port = " + tcp.destination() + "; tcp.src_port = " + tcp.source() + "; tcp.ack = " + tcp.ack()  + "\n");
+//            System.out.printf("tcp.dst_port = " + tcp.destination() + "; tcp.src_port = " + tcp.source() + "; tcp.ack= " + tcp.ack());
+        }
+        if (packet.hasHeader(Udp.ID)) {
+            packet.getHeader(udp1);
+            printResult = printResult + "> udp.dst_port = " + udp1.destination() + "; udp.src_port = " + udp1.source()  + "\n";
+//            textPane1.setText(textPane1.getText() + "> udp.dst_port = " + udp1.destination() + "; udp.src_port = " + udp1.source()  + "\n");
+//            System.out.printf("udp.dst_port = ", udp1.destination() + "; udp.src_port=%d%n", udp1.source());
+        }
+        textPane1.setText(textPane1.getText() + printResult + "Frame " + packet.getFrameNumber() + " \n" );
+        textPane1.setCaretPosition(textPane1.getDocument().getLength());
+        //System.out.printf("frame #%d%n", packet.getFrameNumber());
     }
 
 }
